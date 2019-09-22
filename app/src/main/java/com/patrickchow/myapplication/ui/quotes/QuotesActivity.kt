f30e45a3ecc4fa723502d2d38ba4090f894cae65
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-21 21:08:32 -0400

    Added a function to initializeUI

--- app/src/main/java/com/patrickchow/myapplication/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/com/patrickchow/myapplication/ui/quotes/QuotesActivity.kt
@@ -2,12 +2,41 @@ package com.patrickchow.myapplication.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.patrickchow.myapplication.R
+import com.patrickchow.myapplication.data.Quote
+import com.patrickchow.myapplication.utilities.InjectorUtils
+import kotlinx.android.synthetic.main.activity_quotes.*
+import java.lang.StringBuilder
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
+        initializeUi()
+    }
+
+    private fun initializeUi(){
+        val factory = InjectorUtils.provideQuoteViewModelFactory()
+        val viewModel = ViewModelProviders.of(this, factory)
+            .get(QuotesViewModel::class.java)
+
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach {
+                stringBuilder.append("$it\n\n")
+            }
+            textView_quotes.text = stringBuilder.toString()
+        })
+
+        button_add_quote.setOnClickListener {
+            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
+            viewModel.addQuotes(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
     }
 }
